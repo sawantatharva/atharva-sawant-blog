5b42e8848259271a09f68c98514c9196b375cdbc
Atharva Sawant <87320313+sawantatharva@users.noreply.github.com>
2022-01-11 09:32:22 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
+      <Seo title="All Posts" />
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
