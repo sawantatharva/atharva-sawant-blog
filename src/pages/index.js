d578ae48fd96f2405ff051fa53a8c20f8bbf1711
Atharva Sawant <87320313+sawantatharva@users.noreply.github.com>
2022-01-11 13:20:53 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,10 +44,6 @@ const BlogIndex = ({ data, location }) => {
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <h4>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                  </h4>
                   <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
